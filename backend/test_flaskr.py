d272dce8583facb8f9c721ef4c159e76c12e9e19
nnekasandra <nnekasandra@gmail.com>
2022-09-17 14:12:49 +0100

    started writing tests, wrote tests for get, post and delete api endpoints

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -19,7 +19,6 @@ class TriviaTestCase(unittest.TestCase):
         self.database_path = "postgresql://{}:{}@{}/{}".format(
             "postgres", "nneka2000", "localhost:5432", self.database_name
         )
-        # self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -49,13 +48,54 @@ class TriviaTestCase(unittest.TestCase):
     """
     def test_question_retrival(self):
         response = self.client().get('/questions')
-        data = json.loads(response)
+        data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data["success"], True)
 
+    def test_question_retrival_exceeding_available_page(self):
+        response = self.client().get('/questions?page=200')
+        data = json.loads(response.data)
+        
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable entity')
 
+    def test_categories_retrival(self):
+        response =self.client().get('/categories')
+        data = json.loads(response.data)
 
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["success"], True)
+
+    # def test_delete_question(self):
+    #     response = self.client().delete('/questions/5') 
+    #     data = json.loads(response.data) 
+
+    #     question = Question.query.filter(Question.id == 5).one_or_none()
+
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(data["success"], True)
+    #     self.assertEqual(data["question_deleted"], 5)   
+
+    def test_delete_question_not_found(self):
+        response = self.client().delete('/questions/5') 
+        data = json.loads(response.data) 
+
+        question = Question.query.filter(Question.id == 5).one_or_none()
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], 'unprocessable entity')
+
+    def test_create_new_question(self):
+        response = self.client().post('/questions', json=self.new_question)
+        data = json.loads(response.data) 
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["success"], True)   
+        self.assertTrue(data["question_id"])
+        self.assertTrue((data["total_question"]))
+           
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
